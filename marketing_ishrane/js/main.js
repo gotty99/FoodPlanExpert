6895674d8cee2e15298c8d94928bb79692b8e938
Gotty99 <veljkovicognjen99@gmail.com>
2020-07-04 14:56:13 +0200

    First commit

---
+++ marketing_ishrane/js/main.js
@@ -0,0 +1,8 @@
+// document.getElementById("toggImg").addEventListener("click", function () {
+//   var nav1 = document.getElementById("toggNav");
+//   if (nav1.style.display == "none") {
+//     nav1.style.display == "block";
+//   } else {
+//     nav1.style.display == "none";
+//   }
+// })
